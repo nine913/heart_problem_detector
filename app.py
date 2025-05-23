1a5a0e58bdd0400984c83d8ab600105f30da03e0
nine913 <abbacchio91@gmail.com>
2025-05-22 21:17:46 -0400

    minor changes

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+st.set_page_config(layout="wide",
+    page_title="Heart Problem Detector",
+    page_icon="❤️")
 
 model = joblib.load("heart_pipeline.pkl")
 
